4c914dcf0c51d738037a995150782523fcf59a79
yashpatil1212 <ydp131225@gmail.com>
2026-05-31 17:39:34 +0530

    arithmatic program

---
+++ arithmatic.py
@@ -0,0 +1,23 @@
+no1 =10
+no2 =8
+res1=no1+no2
+print("res1=",res1)
+
+res2=no1-no2
+print("res2=",res2)
+
+res3=no1*no2
+print("res3=",res3)
+
+res4=no1/no2
+print("res4=",res4)
+
+res5=no1%no2
+print("res5=",res5)
+
+res6=no1//no2
+print("res6=",res6)
+
+res7=no1**no2
+print("res7=",res7)
+
