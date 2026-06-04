fbe2f12b677980aa365336dcf5f826d1cb8a0051
yashpatil1212 <ydp131225@gmail.com>
2026-06-04 22:03:35 +0530

    Calculator program
    Basic level program

---
+++ calculator.py
@@ -0,0 +1,11 @@
+no1=float(input("enter no1="))
+no2=float(input("enter no2="))
+
+add=no1+no2
+print("addition is=",add)
+mul=no1*no2
+print("multiplication is=",mul)
+div=no1/no2
+print("division is=",div)
+sub=no1-no2
+print("subtraction is=",sub)
