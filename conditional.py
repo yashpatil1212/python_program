07bb039e3aa80f1a303f57273bc9dc1b168cc083
yashpatil1212 <ydp131225@gmail.com>
2026-06-05 16:55:00 +0530

    conditional statement
    write a program to check if a number is positive, negative or zero. taking input from the user.

---
+++ conditional.py
@@ -0,0 +1,7 @@
+#write a program to check if a number is positive, negative or zero#taking input from the user
+num = float(input("Enter a number: "))
+
+if num>0:
+    print("Number is positive")
+else:
+    print("Number is negative")
