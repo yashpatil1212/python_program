b57cd5a60ee9eda0b9363023d70c03a6d763b441
yashpatil1212 <ydp131225@gmail.com>
2026-06-05 17:22:51 +0530

    Simple problem
    Write a program to check whether a number is odd or even.taking input from the user.

---
+++ conditional2.py
@@ -0,0 +1,8 @@
+#Write a program to check whether a number is odd or even.
+#taking input from the user
+num = int(input("Enter a any  Number:"))
+
+if num%2==0:
+    print("Number is even")
+else:
+    print("Number is odd")
