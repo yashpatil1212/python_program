2e72e6acf05a83314f8bf6152673c9a645fb2cc0
yashpatil1212 <ydp131225@gmail.com>
2026-06-10 17:44:52 +0530

    problem
    problem solving

---
+++ loops problem.py
@@ -0,0 +1,22 @@
+a ="why fit in ,When you are Born to Stand Out !"
+
+#write a program to find the length of the following string
+b=(len(a))
+print("the length of the given string is ",b) 
+
+#write a program to check how many times alphabet o is occuring.
+print("the numbers of times o is occuring is ",a.count("O"))
+
+#write a program to convert the whole string into lower case and upper case.
+x =a.lower()
+print(x)
+y=a.upper()
+print(y)
+
+#write a program to convert the following string into title.
+z=a.title()
+print(z)
+
+#write a program to find th eindex numbers of "fit in".
+
+print(a.find("fit in"))
