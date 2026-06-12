8f13def16aa006011493e3390cb245c4868726d8
yashpatil1212 <ydp131225@gmail.com>
2026-06-12 20:49:51 +0530

    String
    String Methods

---
+++ stringmethods2.py
@@ -0,0 +1,105 @@
+a="hello"
+b="123Hello" 
+c="123456"
+d="HELLO"
+e=" "
+f="Hello 123"
+g="1.234"
+h="Herry Potter And The Goblet Of Fire"
+#isalnum-Returns True idf all characters in the string are alphanumeric
+
+print("isalnum method results:")
+print(a,a.isalnum()) 
+print(b,b.isalnum())
+print(c,c.isalnum()) 
+print(d,d.isalnum()) 
+print(e,e.isalnum()) 
+print(f,f.isalnum()) 
+print(g,g.isalnum())
+
+#isalpha-Returns True if all characters in the string are in the alphabet
+
+print("\n isalpha method results:")
+print(a,a.isalpha())
+print(b,b.isalpha())
+print(c,c.isalpha())            
+print(d,d.isalpha())
+print(e,e.isalpha())
+print(f,f.isalpha())
+print(g,g.isalpha())
+
+
+
+#isdecimal-Returns True if all characters in the string are decimals
+
+print("\n isdecimal method results:")
+print(a,a.isdecimal())
+print(b,b.isdecimal())
+print(c,c.isdecimal())
+print(d,d.isdecimal())
+print(e,e.isdecimal())
+print(f,f.isdecimal())
+print(g,g.isdecimal())
+
+
+
+#isdigit-Returns True if all characters in the string are digits 
+print("\n isdigit method results:")
+print(a,a.isdigit())
+print(b,b.isdigit())
+print(c,c.isdigit())
+print(d,d.isdigit())
+print(e,e.isdigit())
+print(f,f.isdigit())
+print(g,g.isdigit())
+#isnumeric-Returns True if all characters in the string are numeric
+print("\n isnumeric method results:")   
+print(a,a.isnumeric())
+print(b,b.isnumeric())
+print(c,c.isnumeric())
+print(d,d.isnumeric())
+print(e,e.isnumeric())
+print(f,f.isnumeric())
+print(g,g.isnumeric())
+
+#islower-check if the string is lower case or not 
+
+print("\n islower method results:")
+print(a,a.islower())
+print(b,b.islower())
+print(c,c.islower())
+print(d,d.islower())
+print(e,e.islower())
+print(f,f.islower())
+print(g,g.islower())
+
+#isupper-returns True if all characters in the string are upper case
+print("\n isupper method results:")
+print(a,a.isupper())
+print(b,b.isupper())
+print(c,c.isupper())
+print(d,d.isupper())
+print(e,e.isupper())
+print(f,f.isupper())
+print(g,g.isupper())
+
+#isspace-Returns True if all characters in the string are whitespaces
+print("\n isspace method results:")
+print(a,a.isspace())
+print(b,b.isspace())
+print(c,c.isspace())
+print(d,d.isspace())
+print(e,e.isspace())
+print(f,f.isspace())
+print(g,g.isspace())
+
+#istitle-Returns True if the string follows the rules of a title
+print("\n istitle method results:")
+print(a,a.istitle())
+print(b,b.istitle())
+print(c,c.istitle())
+print(d,d.istitle())
+print(e,e.istitle())
+print(f,f.istitle())
+print(g,g.istitle())
+print(h,h.istitle())
