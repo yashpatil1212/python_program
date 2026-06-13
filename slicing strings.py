038e5dc2af5d06527b3a4069d87d4021ff51ce72
yashpatil1212 <ydp131225@gmail.com>
2026-06-13 14:51:37 +0530

    Create slicing strings.py
    slicing methods

---
+++ slicing strings.py
@@ -0,0 +1,11 @@
+#string Slicing
+a="I am learning python"
+b="0123456789"
+print(a)
+print(b)
+print(a[0:5])  # output: I am
+print(a[5:14])  # output: learning
+print(a[-7:])  # output: python
+print(b[::2])  # output: 02468
+print(b[:7:2])  # output: 0246
+print(b[::-1])  # output: 9876543210
