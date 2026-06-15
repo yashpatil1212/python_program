1db296ae000ddf75339a7f6808ae5257b58413f8
yashpatil1212 <ydp131225@gmail.com>
2026-06-15 22:04:46 +0530

    Create problemsolved.py
    problem

---
+++ problemsolved.py
@@ -0,0 +1,31 @@
+#A="OOTD.YOLO.ASAP.BRB.GTG.OTW"
+#1.Write a program to separate the following string into coma(,)separated values.
+
+a="OOTD.YOLO.ASAP.BRB.GTG.OTW"
+b=a.split(".")
+print(b)
+
+#2.write a program to sort string alphabetically in python.
+print("SECOND ANS:")
+b="yash"
+b=sorted(b)
+print(b)
+
+#3.write a program to remove a given character from a string.
+print("THIRD ANS")
+a="hello"
+b=a.replace("e","")
+print(b)
+
+#Z="F.R.I.E.N.D.S."
+#4.write a program to remove dot(.)from the following string.
+print("FOURTH ANS:")
+z="F.R.I.E.N.D"
+b=z.replace(".","")
+print(b)
+
+#5.write a program to check the number of occurrence of a substring in a string.
+print("FIVE ANS:")
+a="ahe sells seashells on the sea shore"
+b=a.count("sea")
+print("The number of time string occuring SEA",b)
