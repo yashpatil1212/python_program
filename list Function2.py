6f144252739c8033ea7f79de6e77ae88bb8585f2
yashpatil1212 <ydp131225@gmail.com>
2026-06-21 19:34:52 +0530

    Create list Function2.py
    list Function part 2

---
+++ list Function2.py
@@ -0,0 +1,27 @@
+a=["yash","kuldip","gaurav","nitin","yash"]
+#to create a copy of a list
+b=[]
+print(b)#check the copy
+b=a.copy()
+print(b)
+
+#to access an element
+print(a.index("yash"))
+
+#to entend the list 
+c=["mayur","chetan"]
+a.extend(c)
+print(a)
+
+#to reverse the list
+a.reverse()
+print(a)
+
+#to sort the list
+a.sort()
+print(a)
+
+#to clear all the data from the list
+a.clear()
+print(a)
+
