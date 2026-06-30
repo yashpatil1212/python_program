c0ea1bb90031f993b4b5516d14bc06c02bf7e034
yashpatil1212 <ydp131225@gmail.com>
2026-06-30 16:05:08 +0530

    Create slicing and iteration tuple.py
    slicing and iteration tuple.py

---
+++ slicing and iteration tuple.py
@@ -0,0 +1,16 @@
+a=("oneplus","vivo","redmi","sumsung","nokia")
+
+#with for loop
+for i in a :
+    print(i)
+
+
+#along with range and length in for loop
+for i in range (len(a)):
+    print(a[i])
+
+#along with while loop
+i=0
+while i<len(a):
+    print(a[i])
+    i+=1
